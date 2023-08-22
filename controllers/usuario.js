342eb181ec22d9464a647286583473c7e9d6bc6a
Nicolas Coiado <nicolasmcoiado@gmail.com>
2023-08-21 22:40:44 -0300

    Estruturando o cadastro de usuários

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,4 +1,5 @@
 import db from "../config/db.js";
+import bcrypt from "bcryptjs";
 
 export const getUsuarios = (_, res) => {
   const sql = "SELECT * FROM usuario";
@@ -7,6 +8,26 @@ export const getUsuarios = (_, res) => {
       return res.status(404).json(error);
     } else {
       return res.status(200).json(results);
+    }
+  });
+};
+
+export const addUsuario = (req, res) => {
+  const sql =
+    "INSERT INTO usuario (nome_usuario, email, senha, telefone) VALUES (?)";
+
+  const valores = [
+    req.body.nome_usuario,
+    req.body.email,
+    bcrypt.hashSync(req.body.senha),
+    req.body.telefone,
+  ];
+
+  db.query(sql, [valores], (error, results) => {
+    if (error) {
+      return res.status(404).json(error);
+    } else {
+      return res.status(200).json("Usuário criado com sucesso.");
     }
   });
 };
--- routes/usuario.js
+++ routes/usuario.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUsuarios } from "../controllers/usuario.js";
+import { getUsuarios, addUsuario } from "../controllers/usuario.js";
 
 const routes = express.Router();
 
-routes.get("/", getUsuarios);
+routes.get("/user", getUsuarios);
+routes.post("/cadastro", addUsuario);
 
 export default routes;
