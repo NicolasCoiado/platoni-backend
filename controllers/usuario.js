08287eb4e2e1882a47c60786205399078b0f50fc
Nicolas Coiado <nicolasmcoiado@gmail.com>
2023-08-26 23:52:49 -0300

    Implementando o login de usuários

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,5 +1,7 @@
 import db from "../config/db.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 export const getUsuarios = (_, res) => {
   const sql = "SELECT * FROM usuario";
@@ -22,9 +24,39 @@ export const addUsuario = async (req, res) => {
 
   db.query(sql, [nome_usuario, email, senha, telefone], (error, results) => {
     if (error) {
-      return res.status(400).json(error);
+      return res.status(402).json(error);
     } else {
       return res.status(201).json("Usuário criado com sucesso.");
+    }
+  });
+};
+
+export const login = async (req, res) => {
+  const { email, senha } = req.body;
+  const sql = "SELECT * FROM usuario WHERE usuario.email=?";
+  db.query(sql, [email], async (erro, usuario) => {
+    if (!usuario[0]) {
+      return res.status(422).json({ msg: "Usuário não encontrado!" });
+    } else {
+      const checkSenha = bcrypt.compareSync(senha, usuario[0].senha);
+      if (!checkSenha) {
+        return res.status(422).json({ msg: "Senha incorreta!" });
+      } else {
+        try {
+          const secret = process.env.SECRET;
+          const token = jwt.sign(
+            {
+              id: usuario[0].id,
+            },
+            secret
+          );
+          res
+            .status(200)
+            .json({ msg: "Autenticação realizada com sucesso", token });
+        } catch (erro) {
+          console.log(erro);
+        }
+      }
     }
   });
 };
--- routes/usuario.js
+++ routes/usuario.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getUsuarios, addUsuario } from "../controllers/usuario.js";
+import { getUsuarios, addUsuario, login } from "../controllers/usuario.js";
 
 const routes = express.Router();
 
 routes.get("/user", getUsuarios);
 routes.post("/cadastro", addUsuario);
-
+routes.post("/", login);
 export default routes;
