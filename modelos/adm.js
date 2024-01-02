ab6a4bf3d1b6a802616a2fd50178b30b593467ee
Nicolas Coiado <nicolascoiado03@hotmail.com>
2024-01-02 20:45:11 -0300

    Finalizando a implementação de banimentos na plataforma.

--- modelos/adm.js
+++ modelos/adm.js
@@ -18,5 +18,18 @@ export const banir = async (req, res) => {
     const deletCert = "DELETE FROM certificados WHERE id_usuario=?;"
     const deletUsuario = "DELETE FROM usuarios WHERE id_usuario=?;"
 
-    
+    db.query(deletCert, id_usuario, async (erro, resultado) => {
+        if(erro){
+            return res.status(400).json({msg: "Erro ao excluir certificados do usuário.", erro})
+        }else{
+            db.query(deletUsuario, id_usuario, async (err, resultad) => {
+                if(erro){
+                    return res.status(400).json({msg: "Erro ao excluir usuário.", err})
+                }else{
+                    return res.status(200).json({msg: "Usuário excluído."})
+                }
+            })
+        }
+    })
+
 }
--- rotas/rota-adm.js
+++ rotas/rota-adm.js
@@ -2,11 +2,13 @@ import express from "express"
 import verificaAdm from "../configs/verifica-adm.js"
 
 import {
-    getUsuarios
+    getUsuarios,
+    banir
 } from "../modelos/adm.js"
 
 const routesAdm = express.Router()
 
 routesAdm.get("/get_usuarios", verificaAdm, getUsuarios)
+routesAdm.delete("/banir", verificaAdm, banir)
 
 export default routesAdm
