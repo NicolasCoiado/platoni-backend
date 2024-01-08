a1b0a3ba6e84f5346894ea15381452b78334d56e
Nicolas Coiado <nicolascoiado03@hotmail.com>
2024-01-07 21:47:39 -0300

    Iniciando documentação do projeto.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 import express from "express"
+import swaggerUi from "swagger-ui-express"
 import routesUser from "./api/routes/rota-usuario.js"
 import routesAdm from "./api/routes/rota-adm.js"
 import routesCert from "./api/routes/rota-certificado.js"
-
+import fs from 'fs';
 import "dotenv/config"
+
+const rawdata = fs.readFileSync('./docs/swagger.json');
+const swaggerDocument = JSON.parse(rawdata);
 
 const app = express()
 const PORT = process.env.PORT
 
 app.use(express.json())
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(routesUser)
 app.use(routesAdm)
